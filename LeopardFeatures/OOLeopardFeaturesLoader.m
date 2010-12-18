69f010774f65f36db1c69b58e75db10d567c6371
Jens Ayton <jens@ayton.se>
2010-12-18 15:41:07 +0000

    Proper header comments for Leopard support bundle; added Leopard support bundle to newjs project.

--- LeopardFeatures/OOLeopardFeaturesLoader.m
+++ LeopardFeatures/OOLeopardFeaturesLoader.m
@@ -1,10 +1,29 @@
-//
-//  OOLeopardFeaturesLoader.m
-//  LeopardFeatures
-//
-//  Created by Jens Ayton on 2010-04-02.
-//  Copyright 2010 Jens Ayton. All rights reserved.
-//
+/*
+OOLeopardFeaturesLoader.m
+Oolite
+ 
+
+Copyright (C) 2008-2010 Jens Ayton
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 
 #import "OOLeopardFeaturesLoader.h"
 #import "OOLogging.h"
@@ -15,15 +34,7 @@
 
 - (id) init
 {
-	if ([JoystickHandler setStickHandlerClass:[JoystickHandlerOSXLeopard class]])
-	{
-		OOLog(@"temp.joystick", @"Successfully installed joystick handler.");
-	}
-	else
-	{
-		OOLog(@"temp.joystick", @"Failed to install joystick handler.");
-	}
-
+	[JoystickHandler setStickHandlerClass:[JoystickHandlerOSXLeopard class]];
 	
 	return [super init];
 }

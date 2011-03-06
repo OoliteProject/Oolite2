35676f58a01682c981d1d55b806b10ccf8402dd4
Jens Ayton <jens@ayton.se>
2011-03-06 14:53:27 +0100

    Missing change from revision updating to FF4b12.

--- Oolite/Source/Scripting/OOJSScript.m
+++ Oolite/Source/Scripting/OOJSScript.m
@@ -74,7 +74,7 @@ static JSClass sScriptClass =
 	JS_PropertyStub,
 	JS_PropertyStub,
 	JS_PropertyStub,
-	JS_PropertyStub,
+	JS_StrictPropertyStub,
 	JS_EnumerateStub,
 	JS_ResolveStub,
 	JS_ConvertStub,
